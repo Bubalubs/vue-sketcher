7c99571b4d2ce7e0ecd0392cc461cb57df5b838c
Adam <4392150+Bubalubs@users.noreply.github.com>
2025-01-28 21:07:11 +0000

    Add Github stars/link to demo

--- demo/App.vue
+++ demo/App.vue
@@ -4,6 +4,18 @@
             Vue Sketcher Demo
         </h1>
 
+        <a
+            href="https://github.com/Bubalubs/vue-sketcher"
+            target="_blank"
+            style="display: block; text-align: center; margin: 20px 0"
+            title="GitHub Repository"
+        >
+            <img
+                src="https://img.shields.io/github/stars/Bubalubs/vue-sketcher?style=social"
+                alt="GitHub stars"
+            />
+        </a>
+
         <VueSketcher :canvasWidth="800" :canvasHeight="500" />
     </div>
 </template>
